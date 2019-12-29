845abed2292141ac9a0c77d6ae74bfb56867ee0c
Skitals <hondacrxsi@gmail.com>
2019-12-29 12:10:02 -0500

    Update enableDark.sh

--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
+++ source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
@@ -2,4 +2,5 @@
 cp /usr/local/emhttp/plugins/dark.theme/scripts/default-dark.css /usr/local/emhttp/plugins/dynamix/styles/default-dark.css
 cp /usr/local/emhttp/plugins/dynamix/styles/dynamix-black.css /usr/local/emhttp/plugins/dynamix/styles/dynamix-dark.css
 cp /usr/local/emhttp/plugins/dark.theme/scripts/Syslinux.page /usr/local/emhttp/plugins/dynamix/Syslinux.page
+sed -i 's/theme=\".*\"/theme=\"dark\"/g' /boot/config/plugins/dynamix/dynamix.cfg
 echo "enabled";
