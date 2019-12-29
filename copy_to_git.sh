88c487ed8f5193d2f7cd1515b29cc823d6ee7765
Skitals <hondacrxsi@gmail.com>
2019-12-28 19:32:27 -0500

    Update copy_to_git.sh

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p "/tmp/GitHub/disable.security/source/disable.security/usr/local/emhttp/plugins/disable.security/"
-cp /usr/local/emhttp/plugins/disable.security/* /tmp/GitHub/disable.security/source/disable.security/usr/local/emhttp/plugins/disable.security -R -v -p
+mkdir -p "/tmp/GitHub/dark.theme/source/dark.theme/usr/local/emhttp/plugins/dark.theme/"
+cp /usr/local/emhttp/plugins/dark.theme/* /tmp/GitHub/dark.theme/source/dark.theme/usr/local/emhttp/plugins/dark.theme -R -v -p
