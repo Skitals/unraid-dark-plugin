5ac89e8f432017131279068cb5a6a402c9bd164a
Skitals <hondacrxsi@gmail.com>
2019-12-29 12:00:04 -0500

    Update enableDark.sh

--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
+++ source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
@@ -1,11 +1,5 @@
-
 #!/bin/bash
-sed -i 's/pti=off//g' /boot/syslinux/syslinux.cfg
-sed -i 's/mds=off//g' /boot/syslinux/syslinux.cfg
-sed -i 's/spectre_v2=off//g' /boot/syslinux/syslinux.cfg
-sed -i 's/l1tf=off//g' /boot/syslinux/syslinux.cfg
-sed -i 's/nospec_store_bypass_disable//g' /boot/syslinux/syslinux.cfg
-sed -i 's/no_stf_barrier//g' /boot/syslinux/syslinux.cfg
-sed -i 's/mitigations=off//g' /boot/syslinux/syslinux.cfg
-echo "enabled" > /tmp/disable.mitigations.reset
+cp /usr/local/emhttp/plugins/dark.theme/scripts/default-dark.css /usr/local/emhttp/plugins/dynamix/styles/default-dark.css
+cp /usr/local/emhttp/plugins/dynamix/styles/dynamix-black.css /usr/local/emhttp/plugins/dynamix/styles/dynamix-dark.css
+
 echo "enabled";
