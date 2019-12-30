71a506c767831e40d8c23fbfc92a38ae4f1fd4e8
Skitals <hondacrxsi@gmail.com>
2019-12-30 14:09:31 -0500

    Delete copy_to_git.sh

--- copy_to_git.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p "/tmp/GitHub/dark.theme/source/dark.theme/usr/local/emhttp/plugins/dark.theme/"
-cp /usr/local/emhttp/plugins/dark.theme/* /tmp/GitHub/dark.theme/source/dark.theme/usr/local/emhttp/plugins/dark.theme -R -v -p
