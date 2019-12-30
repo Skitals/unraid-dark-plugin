a03a97ba614b02a90bce0e2e05e844fdfda462dc
Skitals <justin.t.nolan@gmail.com>
2019-12-30 13:53:33 -0500

    general fixes

--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/disableDark.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/theme=\".*\"/theme=\"white\"/g' /boot/config/plugins/dynamix/dynamix.cfg
-echo "disabled";
--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
+++ source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
@@ -3,3 +3,13 @@ cp /usr/local/emhttp/plugins/dark.theme/scripts/default-dark.css /usr/local/emht
 cp /usr/local/emhttp/plugins/dynamix/styles/dynamix-black.css /usr/local/emhttp/plugins/dynamix/styles/dynamix-dark.css
 cp /usr/local/emhttp/plugins/dark.theme/scripts/Syslinux.page /usr/local/emhttp/plugins/dynamix/Syslinux.page
 echo "Dark Theme files copied to system.";
+upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
+upMins=$((${upSeconds} / 60))
+if [ "${upMins}" -gt "3" ]
+then
+    echo "Detected fresh install, switching to Dark Theme."
+    sed -i 's/theme=\".*\"/theme=\"dark\"/g' /boot/config/plugins/dynamix/dynamix.cfg
+else
+    echo "Detected fresh boot, preserving your settings."
+fi
+sed -i "s/'black','gray'\]/'black','gray','dark'\]/g" /usr/local/emhttp/login.php
