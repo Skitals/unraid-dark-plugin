825bbd1730037e98ed31a0b2e3e0cb00af9fb4d9
Skitals <hondacrxsi@gmail.com>
2019-12-31 18:46:46 -0500

    Update enableDark.sh

--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
+++ source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp /usr/local/emhttp/plugins/dark.theme/scripts/default-dark.css /usr/local/emhttp/plugins/dynamix/styles/default-dark.css
 cp /usr/local/emhttp/plugins/dynamix/styles/dynamix-black.css /usr/local/emhttp/plugins/dynamix/styles/dynamix-dark.css
+cp /usr/local/emhttp/plugins/dark.theme/scripts/community.applications-dark.css /usr/local/emhttp/plugins/dynamix/styles/community.applications-dark.css
 cp /usr/local/emhttp/plugins/dark.theme/scripts/Syslinux.page /usr/local/emhttp/plugins/dynamix/Syslinux.page
 echo "Dark Theme files copied to system.";
 upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
