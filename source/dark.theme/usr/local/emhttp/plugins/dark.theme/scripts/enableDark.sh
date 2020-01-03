338339247b7480b406e32133a514cda0fd836a53
Skitals <justin.t.nolan@gmail.com>
2020-01-03 14:15:43 -0500

    whoops

--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
+++ source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /usr/local/emhttp/plugins/dark.theme/scripts/default-dark.css /usr/local/emhttp/plugins/dynamix/styles/default-dark.css
 cp /usr/local/emhttp/plugins/dynamix/styles/dynamix-black.css /usr/local/emhttp/plugins/dynamix/styles/dynamix-dark.css
-cp /usr/local/emhttp/plugins/dark.theme/scripts/community.applications-dark.css /usr/local/emhttp/plugins/dynamix/styles/community.applications-dar>
+cp /usr/local/emhttp/plugins/dark.theme/scripts/community.applications-dark.css /usr/local/emhttp/plugins/dynamix/styles/community.applications-dark.css
 cp /usr/local/emhttp/plugins/dark.theme/scripts/Syslinux.page /usr/local/emhttp/plugins/dynamix/Syslinux.page
 sed -i "s/'black','gray'\]/'black','gray','dark'\]/g" /usr/local/emhttp/login.php
