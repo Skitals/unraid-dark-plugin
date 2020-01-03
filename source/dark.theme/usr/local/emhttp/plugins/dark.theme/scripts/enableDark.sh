13e7341e39aa1a814fde1bcfaf00fe1f4c860118
Skitals <justin.t.nolan@gmail.com>
2020-01-03 14:27:30 -0500

    whoops

--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/enableDark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp /usr/local/emhttp/plugins/dark.theme/scripts/default-dark.css /usr/local/emhttp/plugins/dynamix/styles/default-dark.css
-cp /usr/local/emhttp/plugins/dynamix/styles/dynamix-black.css /usr/local/emhttp/plugins/dynamix/styles/dynamix-dark.css
-cp /usr/local/emhttp/plugins/dark.theme/scripts/community.applications-dark.css /usr/local/emhttp/plugins/dynamix/styles/community.applications-dark.css
-cp /usr/local/emhttp/plugins/dark.theme/scripts/Syslinux.page /usr/local/emhttp/plugins/dynamix/Syslinux.page
-sed -i "s/'black','gray'\]/'black','gray','dark'\]/g" /usr/local/emhttp/login.php
