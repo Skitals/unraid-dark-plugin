7ed65a5b88345f9bbbde74823f7f7bacbfa88f3f
Skitals <justin.t.nolan@gmail.com>
2020-01-03 13:48:20 -0500

    switch to update.php method

--- source/dark.theme/usr/local/emhttp/plugins/dark.theme/scripts/disableDark.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?PHP
-function startsWith($haystack, $needle) {
-	if ( !is_string($haystack) || ! is_string($needle) ) return false;
-	return $needle === "" || strripos($haystack, $needle, -strlen($haystack)) !== FALSE;
-}
-$print = exec("sed -i 's/theme=\".*\"/theme=\"white\"/g' /boot/config/plugins/dynamix/dynamix.cfg");
-echo $print;
-?>
